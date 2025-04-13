06cd9c5693111527573a4948a81ec2543e5b8f3b
TriNHM <triminh0502@gmail.com>
2025-04-13 20:37:36 +0700

    [ADD] add state in component

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,12 +2,17 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+  state = {
+    name: "TRI NGUYEN",
+    address: "HCM city",
+    age: 21,
+  };
+
   //JSX
   render() {
     return (
       <div>
-        My first component
-        {Math.random()}
+        My name is: {this.state.name} and I'm from {this.state.address}
       </div>
     );
   }
