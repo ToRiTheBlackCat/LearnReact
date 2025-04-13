ff3d9579e2bfcf068c463e4d6a1cb50b909249eb
TriNHM <triminh0502@gmail.com>
2025-04-13 21:23:45 +0700

    [ADD] add set state to update data

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -8,11 +8,34 @@ class MyComponent extends React.Component {
     age: 21,
   };
 
+  handleClick(event) {
+    console.log("Button clicked");
+    console.log("My name is: " + this.state.name); // chỉ tới element tương tác
+
+    //merge state -> react class: auto knowing which one is new and which one is old to update
+    this.setState({
+      name: "TRI123",
+      age: Math.floor(Math.random() * 100 + 1),
+    });
+  }
+
+  handleOnMouseOver(event) {
+    console.log(event);
+  }
   //JSX
   render() {
     return (
       <div>
-        My name is: {this.state.name} and I'm from {this.state.address}
+        My name is: {this.state.name} and I'm {this.state.age}
+        <br />
+        <button
+          onClick={(event) => {
+            this.handleClick(event);
+          }}
+        >
+          Click me
+        </button>
+        <button onMouseOver={this.handleOnMouseOver}> Hover me</button>
       </div>
     );
   }
