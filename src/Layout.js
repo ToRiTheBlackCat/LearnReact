e0deabb38e66f540bb5d0a3dd83122eb1904fcd2
TriNHM <triminh0502@gmail.com>
2026-01-05 11:07:45 +0700

    [UPDATE] mange components into parent layout component

--- src/Layout.js
+++ src/Layout.js
@@ -9,6 +9,7 @@ import Login from "./components/Auth/Login";
 
 import { ToastContainer, toast, Zoom } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Register from "./components/Auth/Register";
 const Layout = (props) => {
   return (
     <>
@@ -28,6 +29,7 @@ const Layout = (props) => {
         </Route>
 
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
       <ToastContainer
         position="top-right"
