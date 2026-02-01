7836935592246f482fa15f5304f1118af535c7de
TriNHM <triminh0502@gmail.com>
2026-02-01 23:01:06 +0700

    [ADD] handle count dount timer + auto submit

---
+++ src/components/User/Content/CountDown.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+
+const CountDown = (props) => {
+  const [count, setCount] = useState(300);
+
+  useEffect(() => {
+    if (count === 0) {
+      props.onTimeUp();
+      return;
+    }
+
+    const timer = setInterval(() => {
+      setCount(count - 1);
+    }, 1000); //sau 1s chạy 1 lần
+
+    // setTimeout(() => {}); //Chạy đúng 1 lần
+
+    //Clean up
+    return () => {
+      clearInterval(timer);
+    };
+
+    // clearInterval(() => {
+    //   clearInterval(timer);
+    // }, 5000); //Xóa đi lặp vô hạn của setInterval
+  }, [count]);
+
+  const toHHMMSS = (secs) => {
+    const sec_num = parseInt(secs, 10);
+    const hours = Math.floor(sec_num / 3600);
+    const minutes = Math.floor(sec_num / 60) % 60;
+    const seconds = sec_num % 60;
+
+    return [hours, minutes, seconds]
+      .map((v) => (v < 10 ? "0" + v : v))
+      .filter((v, i) => v !== "00" || i > 0)
+      .join(":");
+  };
+
+  return <div className="countdown-container">{toHHMMSS(count)}</div>;
+};
+export default CountDown;
