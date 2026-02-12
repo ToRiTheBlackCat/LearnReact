5517ac173c8e32c8acdce596ca607361453139ad
TriNHM <triminh0502@gmail.com>
2026-02-12 23:26:47 +0700

    [UPDATE] show profile of user

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,6 +8,8 @@ import { doLogout } from "../../redux/action/userAction";
 import { logout } from "../../services/apiServices";
 import { Toast } from "bootstrap";
 import Language from "./Language";
+import Profile from "./ManageProfile/Profile";
+import { useState } from "react";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -15,6 +17,7 @@ const Header = () => {
 
   const account = useSelector((state) => state.user.account);
   const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
+  const [showProfile, setShowProfile] = useState(false);
 
   console.log(">>> check account redux: ", account, isAuthenticated);
 
@@ -36,56 +39,65 @@ const Header = () => {
     }
   };
 
+  const handleClickProfile = () => {
+    setShowProfile(true);
+  };
+
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        {/* <Navbar.Brand href="#home">TRI DEP TRAI</Navbar.Brand> */}
-        <Link to="/" className="navbar-brand">
-          TRI DEP TRAI
-        </Link>
+    <>
+      <Navbar expand="lg" className="bg-body-tertiary" style={{ zIndex: "10" }}>
+        <Container>
+          {/* <Navbar.Brand href="#home">TRI DEP TRAI</Navbar.Brand> */}
+          <Link to="/" className="navbar-brand">
+            TRI DEP TRAI
+          </Link>
 
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Link to="/" className="nav-link">
-              Home
-            </Link>
-            {/* NavLink hỗ trợ className Active (tô đậm chữ khi chọn) */}
-            <NavLink to="/users" className="nav-link">
-              User
-            </NavLink>
-            <NavLink to="/admins" className="nav-link">
-              Admin
-            </NavLink>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Link to="/" className="nav-link">
+                Home
+              </Link>
+              {/* NavLink hỗ trợ className Active (tô đậm chữ khi chọn) */}
+              <NavLink to="/users" className="nav-link">
+                User
+              </NavLink>
+              <NavLink to="/admins" className="nav-link">
+                Admin
+              </NavLink>
 
-            {/* <Nav.Link href="#home">Home</Nav.Link>
+              {/* <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="users">User</Nav.Link>
             <Nav.Link href="admins">Admin</Nav.Link> */}
-          </Nav>
-          <Nav>
-            {!isAuthenticated ? (
-              <>
-                <button className="btn-login" onClick={() => handleLogin()}>
-                  LOGIN
-                </button>
-                <button className="btn-signup" onClick={() => handleSignup()}>
-                  SIGNUP
-                </button>
-              </>
-            ) : (
-              <NavDropdown title="Setting" id="basic-nav-dropdown">
-                <NavDropdown.Item onClick={() => handleLogOut()}>
-                  Log out
-                </NavDropdown.Item>
-                <NavDropdown.Item>Profile</NavDropdown.Item>
-              </NavDropdown>
-            )}
+            </Nav>
+            <Nav>
+              {!isAuthenticated ? (
+                <>
+                  <button className="btn-login" onClick={() => handleLogin()}>
+                    LOGIN
+                  </button>
+                  <button className="btn-signup" onClick={() => handleSignup()}>
+                    SIGNUP
+                  </button>
+                </>
+              ) : (
+                <NavDropdown title="Setting" id="basic-nav-dropdown">
+                  <NavDropdown.Item onClick={() => handleLogOut()}>
+                    Log out
+                  </NavDropdown.Item>
+                  <NavDropdown.Item onClick={() => handleClickProfile()}>
+                    Profile
+                  </NavDropdown.Item>
+                </NavDropdown>
+              )}
 
-            <Language />
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+              <Language />
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Profile show={showProfile} setShow={setShowProfile} />
+    </>
   );
 };
 
